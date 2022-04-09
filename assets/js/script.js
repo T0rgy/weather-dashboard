ae27a880121b7fd95255a7e7848f60f96a4537e7
Torgy <jctorge@gmail.com>
2022-04-08 19:52:32 -0500

    display forcast but no fiveDay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 var key = "6abd422032a07f4d3ac394904d57e5fb"
-var city = "madison"
+var city = "Madison"
 
 var date = moment().format('llll');
 var cityHistory = [];
 var contHistEl = $('.cityHistory');
 var cardTodayBody = $('.cardBodyToday');
-var fiveForecastEl = $('.fiveForecast';)
+var fiveForecastEl = $('.fiveForecast');
 
-$('#search').on('click', function (event){
+$('.search').on('click', function (event){
     event.preventDefault();
     city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
     if (city === "") {
@@ -18,7 +18,7 @@ $('#search').on('click', function (event){
     localStorage.setItem('city', JSON.stringify(cityHistory));
     fiveForecastEl.empty();
     getHistory();
-    getWeatherToday();
+    getCurrentWeather();
 });
 
 function getHistory() {
@@ -26,7 +26,7 @@ function getHistory() {
 
     for (var i = 0; i < cityHistory.length; i++) {
         var rowEl = $('<row>');
-        var btnEl = $('<button>').text('${cityHistory[i]}');
+        var btnEl = $('<button>').text(`${cityHistory[i]}`);
 
         rowEl.addClass('row hisBtnRow');
         btnEl.addClass('btn btn-outline-secondary histBtn');
@@ -42,15 +42,13 @@ function getHistory() {
         event.preventDefault();
         city = $(this).text();
         fiveForecastEl.empty();
-        getWeatherToday();
+        getCurrentWeather();
     });
 };
 
  function getCurrentWeather() {
-    var getUrlCurrent = ("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +"&appid=" + key + "&units=imperial");
-    var getUrlUvi = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily,minutely&appid=" + key);
-    var lon = response.coord.lon;
-    var lat = response.coord.lat;
+    var getUrlCurrent = (`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`);
+    
     
     $(cardTodayBody).empty();
 
@@ -61,14 +59,19 @@ function getHistory() {
         $('.cardTodayCityName').text(response.name);
         $('.cardTodayDate').text(date);
         $('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
-        
-        var pEl = $('<p>').text(`Temperature: ${response.main} °F`);
-        var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
+
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+        var pEl = $('<p>').text(`Temperature: ${response.main.temp}°F`);
+        var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity}%`);
         var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
         
         cardTodayBody.append(pEl);
         cardTodayBody.append(pElHumid);
         cardTodayBody.append(pElWind);
+
+        
+        var getUrlUvi = (`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily,minutely&appid=${key}`);
 
         $.ajax({
             url: getUrlUvi,
@@ -77,6 +80,8 @@ function getHistory() {
             var pElUvi = $('<p>').text("UV Index: ");
             var uviSpan = $('<span>').text(response.current.uvi);
             var uvi = response.current.uvi;
+            pElUvi.append(uviSpan);
+            cardTodayBody.append(pElUvi);
 
             if (uvi >= 0 && uvi <= 2) {
                 uviSpan.attr("class", "green");
@@ -84,17 +89,16 @@ function getHistory() {
                 uviSpan.attr("class", "yellow");
             }else if (uvi > 6 && uvi <= 10) {
                 uviSpan.attr("class", "red");
-            };
+            }
 
-            pElUvi.append(uviSpan);
-            cardTodayBody.append(pElUvi);
+            
         });
     });
     getFiveDayForecast();
 };
 
 function getFiveDayForecast() {
-    var getUrlFiveDay = ("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + key);
+    var getUrlFiveDay = (`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`);
 
     $.ajax({
         url: getUrlFiveDay,
@@ -102,16 +106,60 @@ function getFiveDayForecast() {
     }).then(function(response) {
         var fiveDayArray = response.list;
         var myWeather = [];
-        $.each(fiveDayArray, function(index, value) {
+        $.each(fiveDayArray, function(i, value) {
             testObj = {
                 date: value.dt_txt.split(" ")[0],
-                time:
-            }
+                time: value.dt_txt.split(" ")[1],
+                icon: value.weather[0].icon,
+                temp: value.main.temp,
+                humidity: value.main.humidity,
+                wind: value.wind.speed,
+            };
+             
+            if (value.dt_txt.split(" ")[1] === "12:00:00") {
+                myWeather.push(testObj);
+            };
         })
-    })
-}
+        for (var i = 0; i < myWeather.length; i++) {
+            var divElCard = $('<div>');
+            divElCard.attr('class', 'card text-white bg-primary mb-3 cardOne');
+            divElCard.attr('style', 'max-width: 200px;');
+            fiveForecastEl.append(divElCard);
 
+            var divElHeader = $('<div>');
+            divElHeader.attr('class', 'card-header');
 
-getCurrentWeather('madison');
+            var m = moment(`${myWeather[i].date}`).format('MM-DD-YYYY');
+            divElHeader.text(m);
+            divElCard.append(divElHeader);
 
+            var divElBody = $('<div>');
+            divElBody.attr('class', 'card-body');
+            divElCard.append(divElBody);
 
+            var divElIcon = $('<img>');
+            divElIcon.attr('class', 'icon');
+            divElIcon.attr('src', `https://openweathermap.org/img/wn/${myWeather[i].icon}@2x.png`);
+            divElBody.append(divElIcon);
+
+            var pElTemp = $('<p>').text(`Temperature: ${myWeather[i].temp}°F`);
+            divElBody.append(pElTemp);
+            var pElHumid = $('<p>').text(`Humidity: ${myWeather[i].humidity}%`);
+            divElBody.append(pElHumid);
+            var pElWind = $('<p>').text(`Wind Speed: ${myWeather[i].wind} MPH`);
+            divElBody.append(pElWind);
+        };
+    });
+};
+
+function startCity() {
+    var cityHistoryStore = JSON.parse(localStorage.getItem('city'));
+    if (cityHistoryStore !== null) {
+        cityHistory = cityHistoryStore
+    };
+
+    getHistory();
+    getCurrentWeather();
+};
+
+startCity();
